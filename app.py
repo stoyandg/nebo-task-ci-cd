3b0064fb7c9305eb086cadb514423ea7ca24e45d
Stoyan Georgiev <stoyandg@yahoo.com>
2023-05-19 14:06:12 +0300

    Manual approval test

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 # list of cat images
 images = [
     "https://newsroom.unsw.edu.au/sites/default/files/styles/full_width__2x/public/thumbnails/image/3500028168_c85a03256a_b_2.jpg?itok=LeG6fxCZ",
-    "https://petapixel.com/assets/uploads/2022/11/DSC9127_1.jpeg" 
+    "https://petapixel.com/assets/uploads/2022/11/DSC9127_1.jpeg",
+    "https://images.all-free-download.com/images/graphiclarge/cat_cats_eyes_cat_face_269574.jpg" 
 ]
 @app.route('/')
 def index():
