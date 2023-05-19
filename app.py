f45f8515ed137b86ff7ce9c69f5ff507fea4fd9a
Stoyan Georgiev <stoyandg@yahoo.com>
2023-05-19 15:01:00 +0300

    Final version

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 import random
 app = Flask(__name__)
 # list of cat images
-### TEST
 images = [
     "https://newsroom.unsw.edu.au/sites/default/files/styles/full_width__2x/public/thumbnails/image/3500028168_c85a03256a_b_2.jpg?itok=LeG6fxCZ",
     "https://petapixel.com/assets/uploads/2022/11/DSC9127_1.jpeg",
